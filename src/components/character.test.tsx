be9caef6a0068586964856b32f85553068fa0d00
sjmitch-git <sjmitch20@outlook.com>
2023-12-15 14:47:50 +0000

    Adds api call test

--- src/components/character.test.tsx
+++ src/components/character.test.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
-import { render, fireEvent, screen, waitFor } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { Character } from "./character";
-import { getRoles } from "@testing-library/dom";
 
 const server = setupServer(
   rest.get("https://swapi.dev/api/people/1", (req, res, ctx) => {
@@ -18,18 +17,6 @@ const server = setupServer(
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
-
-/* describe("Character component", () => {
-  it("renders loading state and then the character name", async () => {
-    render(<Character />);
-
-    expect(screen.getByText("Loading...")).toBeInTheDocument();
-
-    await screen.findByRole("heading", { level: 1 });
-
-    expect(screen.getByRole("heading")).toHaveTextContent("Luke Skywalker");
-  });
-}); */
 
 test("renders loading state and then the character name", async () => {
   render(<Character />);
